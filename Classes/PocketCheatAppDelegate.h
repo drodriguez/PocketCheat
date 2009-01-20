326837f2b41781ca5bcfae9259ac5a61783f950c
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-01-20 22:51:24 +0100

    Initial commit.

---
+++ Classes/PocketCheatAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PocketCheatAppDelegate.h
+//  PocketCheat
+//
+//  Created by Daniel Rodríguez Troitiño on 20/01/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PocketCheatAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
