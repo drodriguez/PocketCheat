565b925ad2268393ce7d4d9c36748c84d238a507
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-01-20 23:09:53 +0100

    Added libyaml to the project. Seems to compile ok.

---
+++ libyaml/config.h
@@ -0,0 +1,20 @@
+/*
+ *  config.h
+ *  PocketCheat
+ *
+ *  Created by Daniel Rodríguez Troitiño on 20/01/09.
+ *  Copyright 2009 Daniel Rodríguez Troitiño. All rights reserved.
+ *
+ */
+
+/* Define the major version number. */
+#define YAML_VERSION_MAJOR 0
+
+/* Define the minor version number. */
+#define YAML_VERSION_MINOR 1
+
+/* Define the patch version number. */
+#define YAML_VERSION_PATCH 2
+
+/* Define the version string. */
+#define YAML_VERSION_STRING "0.1.2"
