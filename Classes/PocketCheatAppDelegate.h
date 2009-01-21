923d12781136c3459495e6f9a9a03e99875869bc
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-01-21 01:02:59 +0100

    Copyright line. Test code for YAML parsing. Seems to be working.

--- Classes/PocketCheatAppDelegate.h
+++ Classes/PocketCheatAppDelegate.h
@@ -3,7 +3,7 @@
 //  PocketCheat
 //
 //  Created by Daniel Rodríguez Troitiño on 20/01/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//  Copyright Daniel Rodríguez Troitiño 2009. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
