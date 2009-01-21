46631ebb4f40962aefd24bd62499b5ec33ef3039
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-01-21 21:19:55 +0100

    Base ListController class. Maybe it works for all the views subclassing it.

---
+++ Classes/PCListController.h
@@ -0,0 +1,17 @@
+//
+//  PCListController.h
+//  PocketCheat
+//
+//  Created by Daniel Rodríguez Troitiño on 21/01/09.
+//  Copyright 2009 Daniel Rodríguez Troitiño. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PCListController : UITableViewController {
+ @private
+  NSArray *elements_;
+}
+
+@end
